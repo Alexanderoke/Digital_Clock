c14b9c893adbc7d0659085c94b078319ccdcef3d
alexanderoke <alexander.oke@student.moringaschool.com>
2024-04-14 15:57:41 +0300

    add miliseconds

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let hrs= document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let mil =document.getElementById("milisecs");
 
 setInterval(()=>{
 
@@ -9,5 +10,11 @@ setInterval(()=>{
   hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
   min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
   sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+  
 },1000);
+
+setInterval(()=>{
+  mil.innerHTML=(now.getMilliseconds()<10?"00":"")+now.getMilliseconds();
+},1000);
+
 
